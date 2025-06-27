efaec6873626ffab9420c3f1afc36f49119d6852
Dustin Dobervich <ddobervich@gmail.com>
2025-06-27 17:26:09 -0500

    add tracing

--- src/app.rs
+++ src/app.rs
@@ -1,4 +1,5 @@
 use crate::event::{AppEvent, Event, EventHandler};
+
 use ratatui::{
     crossterm::event::{KeyCode, KeyEvent, KeyModifiers},
     DefaultTerminal,
@@ -13,21 +14,11 @@ pub struct App {
     pub events: EventHandler,
 }
 
-impl Default for App {
-    fn default() -> Self {
-        Self {
-            running: true,
-            events: EventHandler::new(),
-        }
-    }
-}
-
 impl App {
-    /// Constructs a new instance of [`App`].
+    /// Creates a new instance of [`App`] that is intialized to the default state.
     pub fn new() -> Self {
         Self::default()
     }
-
     /// Run the application's main loop.
     pub async fn run(mut self, mut terminal: DefaultTerminal) -> anyhow::Result<()> {
         while self.running {
@@ -45,7 +36,6 @@ impl App {
         }
         Ok(())
     }
-
     /// Handles the key events and updates the state of [`App`].
     pub fn handle_key_events(&mut self, key_event: KeyEvent) -> anyhow::Result<()> {
         match key_event.code {
@@ -58,15 +48,23 @@ impl App {
         }
         Ok(())
     }
-
     /// Handles the tick event of the terminal.
     ///
     /// The tick event is where you can update the state of your application with any logic that
     /// needs to be updated at a fixed frame rate. E.g. polling a server, updating an animation.
     pub fn tick(&self) {}
-
     /// Set running to false to quit the application.
     pub fn quit(&mut self) {
         self.running = false;
+    }
+}
+
+impl Default for App {
+    /// Creates a new default [`App`].
+    fn default() -> Self {
+        Self {
+            running: true,
+            events: EventHandler::new(),
+        }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,67 @@
-use crate::app::App;
-
 mod app;
 mod event;
 mod ui;
 
+use crate::app::App;
+
+use tracing::level_filters::LevelFilter;
+use tracing_subscriber::EnvFilter;
+
+/// Main entry point for the application.
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
+    init_env();
+    run_app().await
+}
+
+/// Initializes the environment that the application will run in.
+fn init_env() {
+    // TODO: before release disable log output by default and make it opt in - for now always output it
+    let dot_env_result = dotenvy::dotenv();
+
+    let remove_result = std::fs::remove_file("output.log");
+
+    let file_writer = tracing_appender::rolling::never(".", "output.log");
+
+    // default to INFO logs but allow the RUST_LOG env variable to override.
+    tracing_subscriber::fmt()
+        .json()
+        .with_writer(file_writer)
+        .with_level(true)
+        .with_target(true)
+        .with_file(true)
+        .with_line_number(true)
+        .with_thread_ids(true)
+        .with_thread_names(true)
+        .with_env_filter(
+            EnvFilter::builder()
+                .with_default_directive(LevelFilter::INFO.into())
+                .from_env_lossy(),
+        )
+        .init();
+
+    // process results after tracing has been initialized
+    match dot_env_result {
+        Ok(path) => tracing::info!("loaded .env file from {}", path.display()),
+        Err(e) => match e {
+            dotenvy::Error::Io(io) if io.kind() == std::io::ErrorKind::NotFound => {
+                tracing::debug!("no .env file found")
+            }
+            _ => tracing::warn!("failed to load .env file: {}", e),
+        },
+    };
+
+    match remove_result {
+        Ok(_) => tracing::debug!("removed log file from previous run"),
+        Err(e) if e.kind() == std::io::ErrorKind::NotFound => {
+            tracing::debug!("no log file from previous run found to remove")
+        }
+        Err(e) => tracing::warn!("failed to remove previous log file: {}", e),
+    }
+}
+
+/// Runs the application.
+async fn run_app() -> anyhow::Result<()> {
     let terminal = ratatui::init();
     let result = App::new().run(terminal).await;
     ratatui::restore();
