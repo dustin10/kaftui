67c701b21a571fb55ebb9b01f78e7c1ae4fe0434
Dustin Dobervich <ddobervich@gmail.com>
2025-08-13 11:30:54 -0500

    make consumer status line widget more generic

--- src/ui/widget.rs
+++ src/ui/widget.rs
@@ -11,39 +11,45 @@ use ratatui::{
 /// A simple [`Widget`] that renders text for the status line in the footer based on the current
 /// [`ConsumerMode`] value for the Kafka consumer.
 #[derive(Builder, Debug)]
-pub struct ConsumerStatusLine<'a, A, B>
+pub struct ConsumerStatusLine<T, F, PR, PA>
 where
-    A: Into<Style> + Clone,
-    B: Into<Style> + Clone,
+    T: AsRef<str> + Clone,
+    F: AsRef<str> + Clone,
+    PR: Into<Style> + Clone,
+    PA: Into<Style> + Clone,
 {
     /// Current [`ConsumerMode`] of the Kafka consumer. Determines the color used to render the
     /// status line text.
     consumer_mode: ConsumerMode,
     /// Topic name that records are being consumed from.
-    topic: &'a str,
+    topic: T,
     /// Any filter that was configured by the user.
-    filter: Option<&'a String>,
+    filter: Option<F>,
     /// Style used for the text when the consumer mode is [`ConsumerMode::Processing`].
-    processing_style: A,
+    processing_style: PR,
     /// Style used for the text when the consumer mode is [`ConsumerMode::Paused`].
-    paused_style: B,
+    paused_style: PA,
 }
 
-impl<'a, A, B> ConsumerStatusLine<'a, A, B>
+impl<T, F, PR, PA> ConsumerStatusLine<T, F, PR, PA>
 where
-    A: Into<Style> + Clone,
-    B: Into<Style> + Clone,
+    T: AsRef<str> + Clone,
+    F: AsRef<str> + Clone,
+    PR: Into<Style> + Clone,
+    PA: Into<Style> + Clone,
 {
     /// Creates a new default [`ConsumerStatusLineBuilder`].
-    pub fn builder() -> ConsumerStatusLineBuilder<'a, A, B> {
+    pub fn builder() -> ConsumerStatusLineBuilder<T, F, PR, PA> {
         ConsumerStatusLineBuilder::default()
     }
 }
 
-impl<'a, A, B> Widget for ConsumerStatusLine<'a, A, B>
+impl<T, F, PR, PA> Widget for ConsumerStatusLine<T, F, PR, PA>
 where
-    A: Into<Style> + Clone,
-    B: Into<Style> + Clone,
+    T: AsRef<str> + Clone,
+    F: AsRef<str> + Clone,
+    PR: Into<Style> + Clone,
+    PA: Into<Style> + Clone,
 {
     /// Draws the status line text based on the current mode of the Kafka consumer.
     fn render(self, area: Rect, buf: &mut Buffer)
@@ -57,13 +63,14 @@ where
 
         let filter_text = self
             .filter
-            .as_ref()
-            .map(|f| format!(" (Filter: {})", f))
+            .map(|f| format!(" (Filter: {})", f.as_ref()))
             .unwrap_or_default();
 
         let paragraph = Paragraph::new(format!(
             "Topic: {} | {:?}{}",
-            self.topic, self.consumer_mode, filter_text,
+            self.topic.as_ref(),
+            self.consumer_mode,
+            filter_text,
         ))
         .style(style);
 
